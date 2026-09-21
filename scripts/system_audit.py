70df09c9e0a8f23b63b5d123a305c86c18244a05
ivanogunbor <153668824+ivanogunbor@users.noreply.github.com>
2026-09-20 20:42:14 -0500

    Add system resource monitoring

--- scripts/system_audit.py
+++ scripts/system_audit.py
@@ -1,8 +1,29 @@
 import socket
 import platform
-hostname = socket.gethostname()
-print("Host Name:",hostname)
-operating_system = platform.system()
-print ("Operating System:", operating_system)
-kernel = platform.release()
-print("Kernel Release:", kernel)
+import shutil
+import psutil
+def get_system_info():
+     hostname = socket.gethostname()
+     print("Host Name:",hostname)
+
+     operating_system = platform.system()
+     print ("Operating System:", operating_system)
+
+     kernel = platform.release()
+     print("Kernel Release:", kernel)
+
+     ip_address = socket.gethostbyname(hostname)
+     print(f"Ip Address: {ip_address}")
+def get_resource_usage():
+     total, used, free = shutil.disk_usage("c:\\")
+     disk_percentage = (used / total) * 100
+     disk_percentage = round(disk_percentage, 1)
+     print(f"Disk Usage {disk_percentage}%")
+
+     memory_percentage = psutil.virtual_memory().percent
+     print(f"Memory Usage: {memory_percentage}%")
+
+get_system_info()
+get_resource_usage()
+
+
