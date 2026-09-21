37e26a32187ea8b2bb83105e6005ff9577e1c0f9
ivanogunbor <153668824+ivanogunbor@users.noreply.github.com>
2026-09-21 18:31:14 -0500

    Added Systems Firewall audit

--- scripts/system_audit.py
+++ scripts/system_audit.py
@@ -2,6 +2,8 @@ import socket
 import platform
 import shutil
 import psutil
+import subprocess
+import json
 def get_system_info():
      hostname = socket.gethostname()
 
@@ -22,12 +24,17 @@ def get_resource_usage():
      total, used, free = shutil.disk_usage("c:\\")
      disk_percentage = (used / total) * 100
      disk_percentage = round(disk_percentage, 1)
-     
+     cpu_percentage = psutil.cpu_percent(interval=1)
      memory_percentage = psutil.virtual_memory().percent
 
+
      resource_usage = {"disk_usage": disk_percentage,
-                     "memory_usage": memory_percentage}
+                        "memory_usage": memory_percentage,
+                        "cpu_usage": cpu_percentage
+                        }
+
      return resource_usage
+
 
 
 system_info = get_system_info()
@@ -35,6 +42,7 @@ resource_usage = get_resource_usage()
 
 memory = resource_usage["memory_usage"]
 disk = resource_usage["disk_usage"]
+cpu = resource_usage["cpu_usage"]
 
 def get_status (percentage):
     if percentage >= 90:
@@ -46,10 +54,50 @@ def get_status (percentage):
     else: 
         return "Ok"
 
+
+
+def get_firewall_status():
+    command ="get-netfirewallprofile | select-object name, Enabled | convertto-json"
+    program = "powershell"
+    result = subprocess.run([program,
+                         "-Command",
+                         command],
+                         capture_output = True,
+                         text = True,)
+    return result
+
+
+
+
+
+
+
+
 memory_status = get_status(memory)
 disk_status = get_status(disk)
+cpu_status = get_status(cpu)
+firewall = get_firewall_status()
+firewall_data = json.loads(firewall.stdout)
+
+
+
+for profile in firewall_data:
+    if profile["Enabled"] == 1:
+        firewall_status = "OK"
+
+    else:
+        firewall_status = "Warning!"
+
+    print(f"{profile['name']}: Firewall [{firewall_status}]")
+
+
+
+
+
+
 
 print (f"Host Name: {system_info['hostname']}")
       
 print (f"Memory Usage: {memory}% [{memory_status}]")
 print (f"Disk Usage: {disk}% [{disk_status}]")
+print (f"CPU Usage: {cpu}% [{cpu_status}] ")
